8d5881f8e03b1fb42405b58d349d9f8d660c253c
Victor Hugo <victorh5900@gmail.com>
2022-03-23 23:59:07 -0400

    feat: adicionado componente do form do registro

---
+++ src/components/RegisterForm.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="container mx-auto px-8">
+    <input type="text">
+    <input type="text">
+    <input type="text">
+    <input type="text">
+    <input type="text">
+    <input type="text">
+  </div>
+</template>
