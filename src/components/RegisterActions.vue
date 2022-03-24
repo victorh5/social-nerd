6e17f73e81b263dfb9e55af04f73137e4a533a61
Victor Hugo <victorh5900@gmail.com>
2022-03-24 02:16:35 -0400

    feat: implementação do register actions, faltando alguns detalhes por conta que não existe tela de login e não existe backend para integração...

---
+++ src/components/RegisterActions.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { PropType } from 'vue';
+import { UserType } from '../views/Register.vue'
+
+const userRegistration = () => {
+  if (props.userInformation.password == props.userInformation.passwordConfirmation) {
+    console.log('Estou me cadastrando no sistema!')
+    console.log(props.userInformation)
+  } else {
+    console.log('Erro no Cadastro!')
+    props.userInformation.passwordConfirmation = ''
+  }
+}
+// usar no lugar da função alreadyHaveAccount <$router.push({ name: 'Login' })>
+const alreadyHaveAccount = () => console.log('Já tenho uma conta')
+
+const props = defineProps({
+  userInformation: {
+    type: Object as PropType<UserType>,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="text-white font-semibold text-xs">
+      <button class="block rounded-lg bg-default-purple w-full py-2.5 mb-2.5" @click="userRegistration">Confirmar Cadastro</button>
+      <button class="block rounded-lg bg-default-gray w-full py-2.5" @click="alreadyHaveAccount">Já tenho uma conta</button>
+    </div>
+</template>
