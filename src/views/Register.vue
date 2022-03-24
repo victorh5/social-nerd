660e415673d4f795ebc0003257878da3822a1e55
Victor Hugo <victorh5900@gmail.com>
2022-03-23 23:59:16 -0400

    feat: criado página de registro

---
+++ src/views/Register.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import Header from '../components/Header.vue'
+import RegisterForm from '../components/RegisterForm.vue'
+import RegisterActions from '../components/RegisterActions.vue'
+</script>
+
+<template>
+  <Header />
+  <div class="container mx-auto px-8">
+    <img src="../assets/astronauta-register.svg" alt="Astronauta Página Registro">
+    <p>Crie já a sua conta e acesse um novo mundo</p>
+    <RegisterForm />
+    <RegisterActions />
+  </div>
+</template>
